cb15f4f33a590984ddddd086522d04ccb1de8aae
vtanguturi <vtanguturi3@gatech.edu>
2017-11-30 03:40:20 -0500

    Add files via upload

--- safeTree.R
+++ safeTree.R
@@ -11,20 +11,19 @@ predictors <- read.csv('~/CX4242/project_files/database/predictors.csv', strings
 main_oob.err = double(nrow(predictors)-2)
 main_test.err = double(nrow(predictors)-2)
 
-for (j in 1:(nrow(industries) - 18)) {
+for (j in 1:(nrow(industries))) {
   industry <- as.character(industries[j,1])
   oob.err = double(nrow(predictors)-2)
   test.err = double(nrow(predictors)-2)
-  #predictors <- read.csv('~/CX4242/project_files/database/predictors.csv', stringsAsFactors = FALSE)
+  predictors <- read.csv('~/CX4242/project_files/database/predictors.csv', stringsAsFactors = FALSE)
   for (i in 2:(nrow(predictors)-1)) {
-    name <- as.character(predictors[i,1])
+    name <- predictors[i,1]
     dataset <- read.csv(paste(c('~/CX4242/project_files/database/Industry/', industry, '/', tolower(name), '_toy.csv'), collapse = ''))
     dataset <- dataset[1:nrow(dataset),2:ncol(dataset)]
     train <- sample(1:nrow(dataset),(floor(2*nrow(dataset)/3)))
     rf <- randomForest(value ~ ., data = dataset, subset= train, importance = TRUE, mtry= ncol(dataset), ntree = 1000)
-    oob.err[i-1] = rf$mse # to account for number of predictors
+    oob.err[i-1] = rf$mse/14 # to account for number of predictors
     prediction <- predict(rf, dataset[-train,])
-    print(dataset[-train])
     test.err[i-1] = with(dataset[-train,], mean( (value - prediction)^2)/14) # to account for number of predictors
     # print(prediction)
     # plot(varImpPlot(rf))
@@ -34,8 +33,8 @@ for (j in 1:(nrow(industries) - 18)) {
     main_oob.err[i-1] <- main_oob.err[i-1] + oob.err[i-1]
     main_test.err[i-1] <- main_test.err[i-1] + test.err[i-1]
   }
-  # print(oob.err)
-  # print(test.err)
+   print(oob.err)
+   print(test.err)
 }
 elements <- nrow(industries) * 14
 main_test.err <- main_test.err/elements
